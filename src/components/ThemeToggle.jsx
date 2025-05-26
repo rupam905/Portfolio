ca04384970ce67900eff192eec77ae81cb3061c7
rupam905 <rupamdutta905.com>
2025-05-26 13:06:02 +0530

    ui changes

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -37,7 +37,7 @@ export const ThemeToggle = () => {
     return (
     <button 
     onClick={toggleTheme}
-    className={cn("fixed max-sm:hidden top-4 right-2 z-50 p-2 rounded-full transition-colors duration-300",
+    className={cn("fixed max-sm:right-20 max-sm:top-5 top-4 right-2 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-hidden","cursor-pointer")}> 
         
         {isDarkMode ? (<Sun className="h-6 w-6 text-yellow-300"/>
